4f17c491a8fa89301b8f4f735bb3f9d136e63534
Elvis Diaz <elvisdiaz020@gmail.com>
2023-10-08 15:34:10 -0400

    adds comments in javascript

--- color-gen.js
+++ color-gen.js
@@ -1,25 +1,21 @@
-
-
-
+// Method One
 const getRandomColorNums = (quantity) => {
-    let allRandoms = [];
-    for (let index = 0; index < quantity; index++) {
-        allRandoms.push(Math.floor(Math.random() * 255));
-    };
-    return allRandoms;
-}
+  let allRandoms = [];
+  for (let index = 0; index < quantity; index++) {
+    allRandoms.push(Math.floor(Math.random() * 255));
+  }
+  return allRandoms;
+};
 
 const changeBackgroundColor = () => {
-    let allRandoms = getRandomColorNums(3);
-    let [first, second, third] = allRandoms;
-    // let randomNum = Math.floor(Math.random() * 16777216)
-    // let firstNum = Math.floor(Math.random() * 255)
-    // let secondNum = Math.floor(Math.random() * 255)
-    // let thirdNum = Math.floor(Math.random() * 255)
-    // document.body.style.backgroundColor = '#' + randomNum
-    document.body.style.backgroundColor = `rgb(${first},${second},${third})`;
+  let allRandoms = getRandomColorNums(3);
+  let [first, second, third] = allRandoms;
+  document.body.style.backgroundColor = `rgb(${first},${second},${third})`;
 
-    console.log(allRandoms);
-}
-
-
+  //   Method Two
+  // let firstNum = Math.floor(Math.random() * 255)
+  // let secondNum = Math.floor(Math.random() * 255)
+  // let thirdNum = Math.floor(Math.random() * 255)
+  //   document.body.style.backgroundColor = `rgb(${firstNum},${secondNum},${thirdNum})`;
+  console.log(allRandoms);
+};
